c2c10cfa1d557ac8add484d35a50692fccdfbde9
Jeffrey Liu <jeffkliu@gmail.com>
2020-03-10 17:00:17 -0400

    created SearchBar component and added to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
    render(){
-      return <div>App</div>
+      return (
+         <div>
+            <div className="ui container">
+               <SearchBar  className="ui"/>
+            </div>
+         </div>
+      ); 
    }
 }
 
